8f21190974a84ed2cb0d71abdd75d31bb51ba1f6
Denis Mikhailov <denis.michailov@gmail.com>
2019-03-27 14:50:18 +1000

    Update InputGroup. Add suffix and prefix

--- src/app/shared/components/input-group/input-group.component.ts
+++ src/app/shared/components/input-group/input-group.component.ts
@@ -7,22 +7,44 @@ import { Component, OnInit, Input, TemplateRef, ContentChild } from '@angular/co
 })
 export class InputGroupComponent implements OnInit {
 
-  @Input() fireAddonBefore: any;
+  @Input() fireAddonBefore: string | TemplateRef<any>;
   addonBefore = false;
   textBefore = false;
-  @Input() fireAddonAfter: any;
+  @Input() fireAddonAfter: string | TemplateRef<any>;
   addonAfter = false;
   textAfter = false;
 
+  @Input() fireSuffix: TemplateRef<any>;
+  @Input() firePrefix: TemplateRef<any>;
+
   liveAddonBeforeTemplate: TemplateRef<any>;
   liveAddonAfterTemplate: TemplateRef<any>;
+  liveSuffixTemplate: TemplateRef<any>;
+  livePrefixTemplate: TemplateRef<any>;
 
   @ContentChild('addonBeforeTemplate') addonBeforeTemplate: TemplateRef<any>;
   @ContentChild('addonAfterTemplate') addonAfterTemplate: TemplateRef<any>;
+  @ContentChild('suffixTemplate') suffixTemplate: TemplateRef<any>;
+  @ContentChild('prefixTemplate') prefixTemplate: TemplateRef<any>;
 
   constructor() { }
 
   ngOnInit() {
+
+    if (this.suffixTemplate) {
+      this.liveSuffixTemplate = this.suffixTemplate;
+    }
+    if (this.prefixTemplate) {
+      this.livePrefixTemplate = this.prefixTemplate;
+    }
+
+    //
+    // TODO:
+    // Здесь на самом деле проще сделать так:
+    // Проверяем, если в компонент передан ng-template с именем addonAfter(Before)TEmplate
+    // то одноименный Input игнорируем.
+    //
+
     if (typeof this.fireAddonBefore === 'object') {
       this.liveAddonBeforeTemplate = this.addonBeforeTemplate;
       this.addonBefore = true;
